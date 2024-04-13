6021f2911a0233b1e594a18b86507c8a2946f1c6
Nicolas Camacho Aguilar <nicolas.aguilar1999@gmail.com>
2024-04-13 00:33:03 -0500

    Music and camera added - Episode 3

--- main.go
+++ main.go
@@ -16,6 +16,11 @@ var (
 	playerSprite rl.Texture2D
 	playerSrc    rl.Rectangle
 	playerDest   rl.Rectangle
+
+	musicPaused bool
+	music       rl.Music
+
+	cam rl.Camera2D
 )
 
 func drawScene() {
@@ -40,21 +45,43 @@ func input() {
 	if rl.IsKeyDown(rl.KeyD) || rl.IsKeyDown(rl.KeyRight) {
 		playerDest.X += playerSpeed
 	}
+
+	if rl.IsKeyPressed(rl.KeyQ) {
+		musicPaused = !musicPaused
+	}
 }
 func update() {
 	running = !rl.WindowShouldClose()
+
+	rl.UpdateMusicStream(music)
+	if musicPaused {
+		rl.PauseMusicStream(music)
+	} else {
+		rl.ResumeMusicStream(music)
+	}
+
+	cam.Target = rl.NewVector2(
+		float32(playerDest.X-(playerDest.Width/2)),
+		float32(playerDest.Y-(playerDest.Height/2)),
+	)
+
 }
+
 func render() {
 	rl.BeginDrawing()
 	rl.ClearBackground(backgroundColor)
+	rl.BeginMode2D(cam)
 
 	drawScene()
 
+	rl.EndMode2D()
 	rl.EndDrawing()
 }
 
 func quit() {
 	rl.UnloadTexture(playerSprite)
+	rl.UnloadMusicStream(music)
+	rl.CloseAudioDevice()
 	rl.CloseWindow()
 }
 
@@ -66,7 +93,21 @@ func init() {
 	playerSprite = rl.LoadTexture("resources/Characters/PlayerSpritesheet.png")
 
 	playerSrc = rl.NewRectangle(0, 0, 48, 48)
-	playerDest = rl.NewRectangle(200, 200, 200, 200)
+	playerDest = rl.NewRectangle(200, 200, 100, 100)
+
+	rl.InitAudioDevice()
+	music = rl.LoadMusicStream("resources/music/AveryFarm.mp3")
+	musicPaused = false
+	rl.PlayMusicStream(music)
+
+	cam = rl.NewCamera2D(
+		rl.NewVector2(float32(screenWidth/2), float32(screenHeight/2)),
+		rl.NewVector2(
+			float32(playerDest.X-(playerDest.Width/2)),
+			float32(playerDest.Y-(playerDest.Height/2)),
+		),
+		0.0,
+		1.0)
 }
 
 func main() {
